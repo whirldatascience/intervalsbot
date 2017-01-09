b51c76db979af50ee72d4315b199478f74957c05
Muthu Pandian <contact@muthupandian.in>
2017-01-10 02:14:32 +0530

    APP

--- application/controllers/Task.php
+++ application/controllers/Task.php
@@ -1,8 +1,8 @@
 <?php
 defined ( 'BASEPATH' ) or exit ( 'No direct script access allowed' );
 class Task extends CI_Controller {
-	public function getTasks($userKey, $password,$limit) {
-		$uri = INTERVALS_API . '/task/?limit='.$limit;
+	public function getTasks($userKey, $password, $limit) {
+		$uri = INTERVALS_API . '/task/?limit=' . $limit;
 		$curl = curl_init ();
 		curl_setopt_array ( $curl, array (
 				CURLOPT_RETURNTRANSFER => 1,
@@ -21,25 +21,57 @@ class Task extends CI_Controller {
 		$responseObj = '{"messages":[
 				{"text" : "Total Count' . $taskCount . '"}]}';
 		
-		echo  ( $responseObj );
+		$responseObj = '{
+ "messages": [
+    {
+      "attachment":{
+        "type":"template",
+        "payload":{
+          "template_type":"generic",
+          "elements":[
+            {
+              "title":"Classic White T-Shirt",
+              "image_url":"http://petersapparel.parseapp.com/img/item100-thumb.png",
+              "subtitle":"Soft white cotton t-shirt is back in style",
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersapparel.parseapp.com/view_item?item_id=100",
+                  "title":"View Item"
+                },
+                {
+                  "type":"web_url",
+                  "url":"https://petersapparel.parseapp.com/buy_item?item_id=100",
+                  "title":"Buy Item"
+                }
+              ]
+            },
+            {
+              "title":"Classic Grey T-Shirt",
+              "image_url":"http://petersapparel.parseapp.com/img/item101-thumb.png",
+              "subtitle":"Soft gray cotton t-shirt is back in style",
+              "buttons":[
+                {
+                  "type":"web_url",
+                  "url":"https://petersapparel.parseapp.com/view_item?item_id=101",
+                  "title":"View Item"
+                },
+                {
+                  "type":"web_url",
+                  "url":"https://petersapparel.parseapp.com/buy_item?item_id=101",
+                  "title":"Buy Item"
+                }
+              ]
+            }
+          ]
+        }
+      }
+    }
+  ]
+}';
 		
-		$jsonData = '{
-			“recipient”:{
-				“id”:”’.$sender.’”
-			},
-			“message”:{
-				“text”:”’.$message_to_reply.’”
-			}
-		}';
+		echo ($responseObj);
 		
-		/*
-		 * {
-		 * "messages": [
-		 * {"text": "Welcome to our store!"},
-		 * {"text": "How can I help you?"}
-		 * ]
-		 * }
-		 */
-		// print_r ( $taskObj );
+		
 	}
 }
